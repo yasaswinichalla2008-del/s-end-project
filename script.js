933016de4444aefc21b1c40efc8906e34c86b523
yasaswinichalla2008-del <yasaswinichalla2008@gmail.com>
2025-11-29 12:27:05 +0530

    v1

---
+++ script.js
@@ -0,0 +1,14 @@
+
+<h2 id="message"></h2>
+
+  // variable
+  let wish = "Enjoy your Holidays!";
+
+  // function to display message
+  function showMessage() {
+    document.getElementById("message").innerText = wish;
+  }
+
+  // call the function
+  showMessage();
+
